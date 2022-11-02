356fa1cadcbbbbf57ef1c2fb362e9c301bd057e4
Matthew Bavosa <matthew.bavosa@gmail.com>
2022-11-02 16:26:26 -0400

    setting up counter app

--- project-purple-cow/src/App.js
+++ project-purple-cow/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/Button";
+import "./assets/css/style.css";
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <div className="count">
+          <h3>Count:</h3>
+          <h1>{count}</h1>
+        </div>
+        <div className="buttons">
+          <Button title={"+"} action={incrementCount} />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
